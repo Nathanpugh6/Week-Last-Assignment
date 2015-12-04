ccfe37399d3a3516e9af298c16ae387fbf66b748
nathanpugh6 <nathanpugh6@gmail.com>
2015-12-04 17:49:03 +0000

    final lab

--- Things.py
+++ Things.py
@@ -16,19 +16,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-def (gCd(x,y):
-     print ("X= ", x, "Y= ", y)
-     if y == 0: #base case here
-         return x
-     else:
-         return (gCd(y,x%y)
-
-def main():
-    x = 252
-    y = 105
-
-    print ("GCD of", x, ",", y, "=", gCd(x,y))
-
-
-if __name__ == "__main__":
-    main()
